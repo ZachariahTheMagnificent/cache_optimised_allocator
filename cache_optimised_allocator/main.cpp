490f4baab7b402c8a190982f7b2ec6a5261f453f
Zachariah the Magnificent <zachariahthemagnificent@gmail.com>
2018-06-21 15:11:48 +0800

    changed project to test C++17 dynamic memory allocation for over-aligned types

--- cache_optimised_allocator/main.cpp
+++ cache_optimised_allocator/main.cpp
@@ -1,17 +1,46 @@
 #include <vector>
-#include "aligned_memory_allocator.hpp"
+#include <iostream>
+
+constexpr auto cache_alignment = size_t { 64 };
+
+struct alignas ( cache_alignment ) vector3
+{
+	float x;
+	float y;
+	float z;
+};
 
 int main ( )
 {
-	using namespace custom_allocators;
+	while ( true )
+	{
+		std::cout << "Testing to see if vector would allocate 12 objects according to their specified alignment.\n";
+		std::cout << '\n';
+		std::vector<vector3> my_vector;
 
-	constexpr auto cache_line_size = size_t { 64 };
-	using CacheAwareAllocator = AlignedMemoryAllocator<cache_line_size>;
+		my_vector.resize ( 12 );
 
-	std::vector<int, CacheAwareAllocator::Type<int>> d;
+		const auto starting_address = reinterpret_cast<std::size_t> ( &my_vector.front ( ) );
 
-	d.resize ( 12 );
+		if ( starting_address %  alignof ( vector3 ) != 0 )
+		{
+			std::cout << "Failure!" << '\n';
+			std::system ( "pause" );
+			break;
+		}
+		else
+		{
+			std::cout << "Success!\nEnter a blank to try again: ";
+			int num = 0;
+			char line [ 256 ];
+			std::cin.getline ( line, 10 );
 
-	std::system ( "pause" );
+			if ( line [ 0 ] != '\0' )
+			{
+				break;
+			}
+		}
+	};
+
 	return 0;
 }
